d0d471d2487ea8b82a93b3890a702be1780559cd
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2024-12-22 20:12:17 +0530

    Create Series Generator.java

---
+++ 22.12.2024/Series Generator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int q = scanner.nextInt(); // Number of queries
+
+        for (int i = 0; i < q; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+
+            // Generate and print the series
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+                sum += (int) Math.pow(2, j) * b;
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close(); // Close the scanner
+    }
+}
